0609954c824d5b884d455c612edfd918adddb4f1
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2024-03-14 23:28:36 +0100

    More refactoring...

--- bpsrec.py
+++ bpsrec.py
@@ -8,24 +8,9 @@ from os.path import exists
 from core import record
 from cli import clifunc
 from cli import cli_run
-from cli import bpsrec_monitor
-from cli import bpsrec_dump
-from cli import bpsrec_delay
-from cli import bpsrec_lifetime
-from cli import bpsrec_background
-from cli import bpsrec_test
-from cli import bpsrec_verbose
-
-
-if __name__ != "__main__":
-    raise ImportError("This file cannot be imported as a python module.")
 
 
 def main(mem):
-    threads = []
-    for monitor, dump in zip(mem["monitors"], mem["dumps"]):
-        args = (monitor, dump, mem["delay"], mem["lifetime"])
-        threads.append(Thread(target=record, args=args, daemon=True))
     is_child = not bool(fork()) if mem["background"] else True
     if not is_child:
         print("[bpsrec] bpsrec is running in the background.")
@@ -33,7 +18,11 @@ def main(mem):
     if mem["verbose"]:
         print(mem)
     try:
-        [thread.start() for thread in threads]
+        threads = []
+        for monitor, dump in zip(mem["monitors"], mem["dumps"]):
+            args = (monitor, dump, mem["delay"], mem["lifetime"])
+            threads.append(Thread(target=record, args=args, daemon=True))
+            thread.start()
         [thread.join() for thread in threads]
     except KeyboardInterrupt:
         pass
@@ -42,14 +31,101 @@ def main(mem):
     exit()
 
 
-memory = {"monitors": [],
-          "dumps": [],
-          "delay": 60,
-          "lifetime": -1,
-          "background": False,
-          "verbose": False}
+@clifunc
+def bpsrec_monitor(call, args, mem):
+    path = str(call)
+    if not exists(path):
+        print(f"[bpsrec] Couldn't find {path}. Skipping to next file.")
+        return
+    mem["monitors"].append(path)
+    if len(mem["monitors"]) == len(mem["dumps"]) + 1:
+        mem["dumps"].append(path + '.bpsrec')
 
-clifuncs = (bpsrec_dump, bpsrec_delay, bpsrec_lifetime, bpsrec_background,
-            bpsrec_test, bpsrec_verbose)
 
-cli_run("bpsrec", iter(argv[1:]), memory, bpsrec_monitor, main, *clifuncs)
+@clifunc
+def bpsrec_dump(call, args, mem):
+    try:
+        if mem["dumps"]:
+            mem["dumps"].pop(-1)
+        mem["dumps"].append(str(next(args)))
+    except (StopIteration, ValueError):
+        print("[bpsrec] The dump option expects a path.")
+        exit()
+
+bpsrec_dump.bindings += ['-o', '--output']
+
+
+@clifunc
+def bpsrec_delay(call, args, mem):
+    try:
+        mem["delay"] = float(next(args))
+    except (StopIteration, ValueError):
+        print("[bpsrec] The delay options expects a numeric.")
+        exit()
+
+bpsrec_delay.bindings += ['-d', '--delay']
+
+
+@clifunc
+def bpsrec_lifetime(call, args, mem):
+    try:
+        mem["lifetime"] = float(next(args))
+    except (StopIteration, ValueError):
+        print("[bpsrec] The lifetime options expects a numeric.")
+        exit()
+
+bpsrec_lifetime.bindings += ['-D', '--duration']
+
+
+@clifunc
+def bpsrec_background(call, args, mem):
+    mem["background"] = True
+    try:
+        mem["lifetime"] = float(next(args))
+    except (StopIteration, ValueError):
+        if mem["lifetime"] >= 0:
+            return
+        else:
+            print("[bpsrec] The background option expects a numeric.",
+                  "Or previously specified non-negative lifetime.")
+            exit()
+
+bpsrec_background.bindings += ['-B', '--background']
+
+
+@clifunc
+def bpsrec_test(call, args, mem):
+    if input("[bpsrec] Enter test mode?",
+             "This will last 15 seconds. [y/N] ") in ["y", "Y"]:
+        return
+    mem["background"] = False
+    mem["monitors"].append("monitor.test")
+    mem["dumps"].append("dump.test")
+    mem["delay"] = 1
+    mem["life"] = 15
+    file = open('monitor.test', 'w')
+    file.close()
+
+bpsrec_test.bindings += ['--test']
+
+
+@clifunc
+def bpsrec_verbose(call, args, mem):
+    mem["verbose"] = True
+
+bpsrec_verbose.bindings += ['-v', '--verbose']
+
+
+if __name__ != "__main__":
+    raise ImportError("This file cannot be imported as a python module.")
+
+memory = cli_run(
+    name="bpsrec",
+    args=iter(argv[1:]),
+    fallback=bpsrec_monitor,
+    memory={"monitors": [], "dumps": [], "delay": 60, "lifetime": -1,
+            "background": False, "verbose": False},
+    clifuncs=[bpsrec_dump, bpsrec_delay, bpsrec_lifetime, bpsrec_background,
+              bpsrec_test, bpsrec_verbose])
+
+main(memory)
--- cli.py
+++ cli.py
@@ -12,9 +12,8 @@ def clifunc(func):
     wrapper.bindings = []
     return wrapper
 
-
 def cli_run(name: str, args: iter, memory: Dict[str, Any], fallback: CliFunc,
-            main: Callable, *clifuncs: CliFunc):
+            clifuncs: list[CliFunc]):
     info = '\n'.join([
         f"{index}.  ({', '.join(func.bindings)}) {func.tooltip}"
         for index, func in enumerate(clifuncs)])
@@ -36,77 +35,4 @@ def cli_run(name: str, args: iter, memory: Dict[str, Any], fallback: CliFunc,
                  "Need help? [y/N] ") in {'y', 'Y'}:
             print(info)
         exit()
-    main(memory)
-
-
-@clifunc
-def bpsrec_monitor(call, args, mem):
-    path = str(call)
-    if not exists(path):
-        print(f"[bpsrec] Couldn't find {path}. Skipping to next file.")
-        return
-    mem["monitors"].append(path)
-    if len(mem["monitors"]) == len(mem["dumps"]) + 1:
-        mem["dumps"].append(path + '.bpsrec')
-
-@clifunc
-def bpsrec_dump(call, args, mem):
-    try:
-        if mem["dumps"]:
-            mem["dumps"].pop(-1)
-        mem["dumps"].append(str(next(args)))
-    except (StopIteration, ValueError):
-        print("[bpsrec] The dump option expects a path.")
-        exit()
-bpsrec_dump.bindings += ['-o', '--output']
-
-@clifunc
-def bpsrec_delay(call, args, mem):
-    try:
-        mem["delay"] = float(next(args))
-    except (StopIteration, ValueError):
-        print("[bpsrec] The delay options expects a numeric.")
-        exit()
-bpsrec_delay.bindings += ['-d', '--delay']
-
-@clifunc
-def bpsrec_lifetime(call, args, mem):
-    try:
-        mem["lifetime"] = float(next(args))
-    except (StopIteration, ValueError):
-        print("[bpsrec] The lifetime options expects a numeric.")
-        exit()
-bpsrec_lifetime.bindings += ['-D', '--duration']
-
-@clifunc
-def bpsrec_background(call, args, mem):
-    mem["background"] = True
-    try:
-        mem["lifetime"] = float(next(args))
-    except (StopIteration, ValueError):
-        if mem["lifetime"] >= 0:
-            return
-        else:
-            print("[bpsrec] The background option expects a numeric.",
-                  "Or previously specified non-negative lifetime.")
-            exit()
-bpsrec_background.bindings += ['-B', '--background']
-
-@clifunc
-def bpsrec_test(call, args, mem):
-    if input("[bpsrec] Enter test mode?",
-             "This will last 15 seconds. [y/N] ") in ["y", "Y"]:
-        return
-    mem["background"] = False
-    mem["monitors"].append("monitor.test")
-    mem["dumps"].append("dump.test")
-    mem["delay"] = 1
-    mem["life"] = 15
-    file = open('monitor.test', 'w')
-    file.close()
-bpsrec_test.bindings += ['--test']
-
-@clifunc
-def bpsrec_verbose(call, args, mem):
-    mem["verbose"] = True
-bpsrec_verbose.bindings += ['-v', '--verbose']
+    return memory
